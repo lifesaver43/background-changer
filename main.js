ccd8beb4315a5a1956ebde7e88f3269c489ba838
lifesaver43 <innocentnwafor17@gmail.com>
2022-10-23 19:25:11 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,62 @@
+const btn = document.querySelector("button");
+const uploadBox = document.querySelector(".upload-box"),
+previewImg = document.querySelector("img"),
+widthInput = document.querySelector(".width input"),
+heightInput = document.querySelector(".height input"),
+fileIput = uploadBox.querySelector("input");
+
+const canv = document.getElementById("myCanvas");
+let canW = canv.width - 40;
+let canH = canv.height - 40;
+let ogImageRatio;
+
+function loadFile(e) {
+    const file = e.target.files[0];
+    if (!file)
+        return;
+    previewImg.src = URL.createObjectURL(file);
+    previewImg.addEventListener("load", () => {
+        widthInput.value = previewImg.naturalWidth;
+        heightInput.value = previewImg.naturalHeight;
+        ogImageRatio = previewImg.naturalWidth / previewImg.naturalHeight;
+        var canvas = document.getElementById("myCanvas");
+        var ctx = canvas.getContext("2d");
+        var img = document.getElementById("scream");
+        ctx.drawImage(img, 20, 20, canW, canH);
+    });
+    console.log(file);
+}
+
+fileIput.addEventListener("change", loadFile);
+
+// window.onload = function() {
+//     var canvas = document.getElementById("myCanvas");
+//     var ctx = canvas.getContext("2d");
+//     var img = document.getElementById("scream");
+//     ctx.drawImage(img, 10, 10, 220, 280);
+
+
+//    const dataURL = canvas.toDataURL();
+//    console.log(dataURL);
+// };
+
+function notRandomBG() {
+    let colorInput = document.getElementById("bgInput1").value;
+    let canvas = document.getElementById("myCanvas");
+
+    canvas.style.background = colorInput;
+}
+
+btn.onclick = function() {
+    let canvas = document.getElementById("myCanvas");
+    let anchor = document.createElement("a");
+    let ctx = canvas.getContext("2d");
+    ctx.globalCompositeOperation = "destination-over";
+    let colorInput = document.getElementById("bgInput1").value;
+    ctx.fillStyle = colorInput;
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+    anchor.href = canvas.toDataURL("image/png");
+    anchor.download = "IMAGE.PNG";
+    anchor.click();
+}
